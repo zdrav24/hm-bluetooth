22ce29a983e16845645610a3d4bb2b1b0679c074
lostrepo <siw2013.com@yandex.ru>
2018-10-15 16:44:59 +0300

    Update Tools.java
    fix array index out of bounds on advertising packets.

--- src/android/org/bcsphere/bluetooth/tools/Tools.java
+++ src/android/org/bcsphere/bluetooth/tools/Tools.java
@@ -736,7 +736,7 @@ public class Tools {
 		JSONArray overflowServiceUUIDs = new JSONArray();
 		boolean isOver = true;
 		while (isOver) {
-			if (!(advData != null)) {
+			if (!(advData.length >= 1))) {
 				isOver = false;
 				break;
 			}
